ae1a4e015363beafe9a5188c8ba13d026a75d5b9
lucas <lucasordeng@gmail.com>
2019-12-10 17:08:01 -0300

    cambio
    ada

--- ServiLand/src/main/java/com/egg/ServiLand/servicios/ClienteServicio.java
+++ ServiLand/src/main/java/com/egg/ServiLand/servicios/ClienteServicio.java
@@ -6,6 +6,7 @@ import com.egg.ServiLand.entidades.Foto;
 import com.egg.ServiLand.errores.ErrorServicio;
 import com.egg.ServiLand.repositorios.ClienteRepositorio;
 import java.util.Date;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -57,8 +58,68 @@ public class ClienteServicio implements UserDetailsService {
         }
     }
 
+@Transactional
+    public void Modificar(MultipartFile archivo, String id, String nombre, String apellido, String mail, String clave) throws ErrorServicio {
+
+        validar(nombre, apellido, mail, clave);
+
+        Optional<Cliente> respuesta = clienteRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+
+            Cliente cliente = clienteRepositorio.findById(id).get();
+            cliente.setNombre(nombre);
+            cliente.setApellido(apellido);
+            cliente.setMail(mail);
+            String encriptada= new  BCryptPasswordEncoder().encode(clave);
+
+            cliente.setClave(encriptada);
+
+            String idFoto = null;
+            if (cliente.getFoto() != null) {
+                idFoto = cliente.getFoto().getId();
+
+                Foto foto = fotoServicio.actualizar(idFoto, archivo);
+                cliente.setFoto(foto);
+                clienteRepositorio.save(cliente);
+
+            } else {
+                throw new ErrorServicio("No se encontró el usuario solicitado");
+            }
+        }
+    }
+
+    @Transactional
+    public void deshabilitar(String id) throws ErrorServicio {
+
+        Optional<Cliente> respuesta = clienteRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+
+            Cliente cliente = clienteRepositorio.findById(id).get();
+            cliente.setBaja(new Date());
+            clienteRepositorio.save(cliente);
+
+        } else {
+            throw new ErrorServicio("No se encontró el usuario solicitado");
+        }
+    }
+@Transactional
+    public void habilitar(String id) throws ErrorServicio {
+
+        Optional<Cliente> respuesta = clienteRepositorio.findById(id);
+        if (respuesta.isPresent()) {
+
+            Cliente cliente = clienteRepositorio.findById(id).get();
+            cliente.setBaja(null);
+            clienteRepositorio.save(cliente);
+
+        } else {
+            throw new ErrorServicio("No se encontró el usuario solicitado");
+        }
+    }
+    
     @Override
     public UserDetails loadUserByUsername(String string) throws UsernameNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
+    
